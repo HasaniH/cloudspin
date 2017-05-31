dc9840606e67f9621ef0672486e0446d617f8d93
Jen Tong <mimming@google.com>
2017-05-31 11:14:57 -0700

    Initial commit of foundation of Android app

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# Cloud Spin Demo
+# Cloud Spin
 
-Watch this space!
-The code for Cloud Spin Demo will be available on this repo soon.
+Cloud Spin is an interactive demo that highlights many awesome features of Google Cloud Platform.
+
+Walk into the semicircle of phones, show off your most exciting moves (jumps look great), and Cloud Spin will use several features of Google Cloud Platform to stitch photos from several Android phones together into an awesome animated gif!
+
+
+## Contributing
+
+Please read and follow the steps in the [CONTRIBUTING.md](CONTRIBUTING.md)
+
+## License
+See [LICENSE](LICENSE)
+
+## Disclaimer
+This is not an official Google product
---
+++ android/cloud_spin_v2/app/src/main/java/com/cloud_spin/hasanih/cloud_spin_v2/MainActivity.java
@@ -0,0 +1,54 @@
+// Copyright 2017 Google Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package com.cloud_spin.hasanih.cloud_spin_v2;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+
+    VideoView result_video;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button click = (Button)findViewById(R.id.videorec);
+        result_video = (VideoView)findViewById(R.id.videoView);
+    }
+
+    public void dispatchTakeVideoIntent(View v) {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            Uri videoUri = data.getData();
+        }
+    }
+}
